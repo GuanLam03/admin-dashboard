5e0e471df4973300043a80198409247c576a6977
Guan Lam <hgledgetech@gmail.com>
2025-12-01 10:34:56 +0800

    document alert notification feature

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -7,13 +7,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./utils/i18n/i18n";
 import { CentrifugeProvider } from "./contexts/CentrifugeContext";
-
+import { ToastContainer } from 'react-toastify';
 // Wrapper to provide OnlineUsersProvider after AuthProvider
 function AppProviders() {
  
   return (
     <CentrifugeProvider>
       <RouterProvider router={router} />
+      <ToastContainer />
     </CentrifugeProvider>
   );
 }
